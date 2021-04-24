668f1efa718bae60514c2b4c7ed8422f02649426
Anas Mohamed <amohamed@ualberta.ca>
2021-04-24 06:19:34 -0600

    identifer and number handling.

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -16,25 +16,13 @@ func New(input string) *Lexer {
 	return l
 }
 
-func (l *Lexer) readChar() {
-	if l.readPosition >= len(l.input) {
-		l.chr = 0
-	} else {
-		l.chr = l.input[l.readPosition]
-	}
-
-	l.position = l.readPosition
-	l.readPosition += 1
-}
-
-func newToken(tokenType token.TokenType, chr byte) token.Token {
-	return token.Token{Type: tokenType, Literal: string(chr)}
-}
-
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhitespace()
+
 	switch l.chr {
+	// Handle known tokens
 	case '=':
 		tok = newToken(token.ASSIGN, l.chr)
 	case ';':
@@ -54,8 +42,71 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.chr) {
+			// Handle identifiers
+			tok.Literal = l.readIdenitifer()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.chr) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			// Token is not know and is therefore illegal
+			tok = newToken(token.ILLEGAL, l.chr)
+		}
 	}
 
 	l.readChar()
 	return tok
+}
+
+func newToken(tokenType token.TokenType, chr byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(chr)}
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.chr = 0
+	} else {
+		l.chr = l.input[l.readPosition]
+	}
+
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) readIdenitifer() string {
+	position := l.position
+
+	for isLetter(l.chr) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+
+	for isDigit(l.chr) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.chr == ' ' || l.chr == '\t' || l.chr == '\n' || l.chr == '\r' {
+		l.readChar()
+	}
+}
+
+func isLetter(chr byte) bool {
+	return 'a' <= chr && chr <= 'z' || 'A' <= chr && chr <= 'Z' || chr == '_'
+}
+
+func isDigit(chr byte) bool {
+	return '0' <= chr && chr <= '9'
 }
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -7,6 +7,74 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
+	input := `let add = fn(a, b) {
+a + b;
+};
+let x = 4;
+let y = 8;
+let result = add(x, y);
+`
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "a"},
+		{token.COMMA, ","},
+		{token.IDENT, "b"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "a"},
+		{token.PLUS, "+"},
+		{token.IDENT, "b"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "x"},
+		{token.ASSIGN, "="},
+		{token.INT, "4"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "y"},
+		{token.ASSIGN, "="},
+		{token.INT, "8"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("test %d: tokentype incorrect. expected=%q, actual=%q",
+				i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("test %d: literal incorrect. expected=%q, actual=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
+
+func TestNextTokenBase(t *testing.T) {
 	input := `=+(){},;`
 	tests := []struct {
 		expectedType    token.TokenType
